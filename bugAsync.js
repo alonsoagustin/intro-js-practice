5cb2a7c7913ff0fbdb7e99a69837dfe0e3769df6
Agustin Alonso <alonsoiagustin@gmail.com>
2024-09-20 17:37:38 +0200

    chore(ejercicio 5): :tada: Add initial setup for exercise 6

---
+++ bugAsync.js
@@ -0,0 +1,20 @@
+/*
+Ejercicio 5 Arreglar bug de asincronía
+
+Tenemos otro error que nuestro cliente nos pide arreglar. 
+El cliente está pidiendo un usuario y nos dice que está usando el id correcto (el 1). Pero siempre le da undefined. Nos ha pasado el código que tenemos que revisar y arreglar.
+Para este problema crear un archivo llamado bugAsync.js con la solución.
+*/
+
+// Este programa simula una llamada asincrónica para obtener un usuario
+function obtenerUsuario(id) {
+  let usuario;
+  setTimeout(() => {
+    if (id === 1) {
+      usuario = { id: 1, nombre: "John Doe" };
+    }
+  }, 2000);
+  return usuario;
+}
+const usuario = obtenerUsuario(1);
+console.log(usuario);
