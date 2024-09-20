7532a802e1c91465b96aec401f39bc1c6a731d95
Agustin Alonso <alonsoiagustin@gmail.com>
2024-09-20 17:10:20 +0200

    feat(ejercicio 3): :sparkles: Add function to convert an array to relative path

--- ejercicio3.js
+++ ejercicio3.js
@@ -9,11 +9,26 @@ necesario usar ningún tipo de condicional.
 3.1 Ejercicio
 Crea una función para que con estos datos de entrada se produzca los siguientes resultados:
 */
+
+const arrayToRelativePath = (array) => {
+  /* 
+    Permite construir el relative path.
+    Recibe un listado como parámetro.
+    Devuelve una cadena con el relative path.
+    Une todos los elementos del listado con un slash, excepto el último, que es unido con un punto.
+  */
+  const extension = array.slice(-1);
+  const located = array.slice(0, array.length - 1);
+  const path = `${located.join("/")}.${extension}`;
+  return path;
+};
+
 const input1 = ["Downloads", "Videos", "capture", "mp4"];
-// create your function here
-yourFunction(input1); // 'Downloads/Videos/capture.mp4'
+arrayToRelativePath(input1); // 'Downloads/Videos/capture.mp4'
+
 const input2 = ["CodinGame", "python", "py"];
-yourFunction(input2); // 'CodinGame/python.py'
+arrayToRelativePath(input2); // 'CodinGame/python.py'
+
 const input3 = [
   "programming",
   "languages",
@@ -23,26 +38,34 @@ const input3 = [
   "pythonstuff",
   "py",
 ];
-yourFunction(input3); // 'programming/languages/easy/beginner/useful/pythonstuff.py'
+arrayToRelativePath(input3); // 'programming/languages/easy/beginner/useful/pythonstuff.py'
+
 /*
 3.2 Ejercicio
 Crea una función para que con estos datos de entrada se produzca los siguientes resultados:
 */
+
 const input4 = 10;
 // create your function here
-yourFunction(input4); // '1-0'
+// yourFunction(input4); // '1-0'
+
 const input5 = 1;
-yourFunction(input5); // '1'
+//yourFunction(input5); // '1'
+
 const input6 = 11234;
-yourFunction(input6); // '1-1-2-3-4'
+//yourFunction(input6); // '1-1-2-3-4'
+
 /*
 3.3 Ejercicio
 Crea una función para que con estos datos de entrada se produzca los siguientes resultados:
 */
+
 const input7 = "string";
 // create your function here
-yourFunction(input7); // '6 gnirts'
+// yourFunction(input7); // '6 gnirts'
+
 const input8 = "variable";
-yourFunction(input8); // '8 elbairav'
+// yourFunction(input8); // '8 elbairav'
+
 const input9 = "pointer";
-yourFunction(input9); // '7 retniop'
+// yourFunction(input9); // '7 retniop'
