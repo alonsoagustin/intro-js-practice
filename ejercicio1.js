d77e4514e52d2a043ba558d24f7c6ee10523925f
Agustin Alonso <alonsoiagustin@gmail.com>
2024-09-20 14:13:00 +0200

    chore(ejercicio 1): :tada: Add initial set up for exercise 1

---
+++ ejercicio1.js
@@ -0,0 +1,9 @@
+/*
+Ejercicio 1
+Crea un archivo ejercicio1.js que tenga un objeto llamado usuario con los siguientes campos:
+- Nombre (el tuyo o inventado)
+- Apellidos (el tuyo o inventado)
+- Una lista con los temas del bootcamp Node.js, Git y react con sus nombres y fechas de inicio de cada módulo. Fecha en formato “YYYY-MM-DD”
+- Si estás en búsqueda activa con un valor de verdadero o false
+En este archivo queremos mostrar por pantalla la fecha de inicio del módulo de react del objeto que hemos creado anteriormente.
+*/
