a04d71b7f3a4ad47c1fe12302d69c08f8d81ff7a
Agustin Alonso <alonsoiagustin@gmail.com>
2024-09-20 16:48:28 +0200

    chore(ejercicio 3): :tada: Add initial setup for exercise 3

---
+++ ejercicio3.js
@@ -0,0 +1,48 @@
+/*
+Ejercicio 3
+En estos ejercicios no tienes acceso al enunciado. Debes deducir qué hacer observando los datos de entrada y el resultado. 
+Lo importante en estos ejercicios es ver el patrón con cada ejemplo. En ningún caso es
+necesario usar ningún tipo de condicional.
+*/
+
+/*
+3.1 Ejercicio
+Crea una función para que con estos datos de entrada se produzca los siguientes resultados:
+*/
+const input1 = ["Downloads", "Videos", "capture", "mp4"];
+// create your function here
+yourFunction(input1); // 'Downloads/Videos/capture.mp4'
+const input2 = ["CodinGame", "python", "py"];
+yourFunction(input2); // 'CodinGame/python.py'
+const input3 = [
+  "programming",
+  "languages",
+  "easy",
+  "beginner",
+  "useful",
+  "pythonstuff",
+  "py",
+];
+yourFunction(input3); // 'programming/languages/easy/beginner/useful/pythonstuff.py'
+/*
+3.2 Ejercicio
+Crea una función para que con estos datos de entrada se produzca los siguientes resultados:
+*/
+const input4 = 10;
+// create your function here
+yourFunction(input4); // '1-0'
+const input5 = 1;
+yourFunction(input5); // '1'
+const input6 = 11234;
+yourFunction(input6); // '1-1-2-3-4'
+/*
+3.3 Ejercicio
+Crea una función para que con estos datos de entrada se produzca los siguientes resultados:
+*/
+const input7 = "string";
+// create your function here
+yourFunction(input7); // '6 gnirts'
+const input8 = "variable";
+yourFunction(input8); // '8 elbairav'
+const input9 = "pointer";
+yourFunction(input9); // '7 retniop'
